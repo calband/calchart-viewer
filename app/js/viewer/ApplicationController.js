e15553bf64981be3f762ab25b6f3611c03557e3d
Noah Gilmore <noah.w.gilmore@gmail.com>
2014-08-15 23:08:29 -0700

    whoops, don't mind that

--- app/js/viewer/ApplicationController.js
+++ app/js/viewer/ApplicationController.js
@@ -47,7 +47,6 @@ ApplicationController.prototype.setShow = function (show) {
     this._show = show;
     this._animationStateDelegate = new AnimationStateDelegate(this._show);
     this._animator.setAnimationStateDelegate(this._animationStateDelegate);
-    this._animationStateDelegate.selectDot("15");
     this._syncWithDelegate();
 };
 
