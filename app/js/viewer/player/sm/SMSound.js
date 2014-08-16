da7f785d525ef02ce366d88e12b1738f58fbf430
Noah Gilmore <noah.w.gilmore@gmail.com>
2014-08-15 23:07:40 -0700

    clean up SMSound. this app is so cool

--- app/js/viewer/player/sm/SMSound.js
+++ app/js/viewer/player/sm/SMSound.js
@@ -20,11 +20,15 @@ var SMSound = function(musicURL) {
      * time 4000 instead of 0, don't throw the timed events occuring before 4000).
      * @type {int}
      */
-    this._startTime = 0; 
+    this._startTime = 0;
+    /**
+     * The actual Sound Manager sound object: this class is a wrapper around it
+     * @type {soundManager.Sound}
+     */
     this._sound = null;
     this._url = musicURL;
     this.reset();
-    if (musicURL != undefined) {
+    if (musicURL !== undefined) {
         this.load(musicURL);
     }
 };
@@ -46,8 +50,11 @@ SMSound.prototype.load = function(musicURL) {
         url: musicURL,
         onplay: this._makeEventRouter("play"),
         onstop: this._makeEventRouter("stop"),
-        onfinish: function() {_this._handleFinished();},
-        onload: function() {_this._destroyURL(); _this._callEventHandler("finishedLoading"); }
+        onfinish: _this._handleFinished,
+        onload: function() {
+            _this._destroyURL();
+            _this._callEventHandler("finishedLoading");
+        }
     });
     this._installTimedEvents();
     this._callEventHandler("startLoading");
@@ -63,9 +70,9 @@ SMSound.prototype.unload = function() {
      * is playing from those thrown when the music is stopped.
      * @type {boolean}
      */
-    this._stopTimedEvents = true; 
+    this._stopTimedEvents = true;
     this._destroyURL();
-    if (this._sound != null) {
+    if (this._sound !== null) {
         this._sound.destruct();
         this._sound = null;
         
@@ -77,53 +84,84 @@ SMSound.prototype.unload = function() {
  * of saving space.
  */
 SMSound.prototype._destroyURL = function() {
-    if (this._url != null) {
+    if (this._url !== null) {
         URL.revokeObjectURL(this._url);
         this._url = null;
     }
 };
 
+/**
+ * Get ride of the soundmanager sound, release the mp3 url, reset all the event
+ * handlers;
+ */
 SMSound.prototype.reset = function() {
     this.unload();
-    this._eventHandlers = [];
-    for (var index = 0; index < this._eventTypes.length; index++) {
-        this._eventHandlers.push(null);
-    }
+    this._eventHandlers = {};
+    /**
+     * An array of objects in which each has a time, in milliseconds, and a
+     * function which will be executed at that time.
+     * @type {Array}
+     */
     this._timedEvents = [];
 };
 
+/**
+ * Play the mp3 fron the given start time.
+ * @param  {int} startTime the time to play the sound at, in milliseconds
+ */
 SMSound.prototype.play = function(startTime) {
     this._stopTimedEvents = false; //Timed events are allowed
-    if (this._sound != null) {
+    if (this._sound !== null) {
         this._startTime = startTime;
         this._sound.play({position: startTime});
     }
 };
 
+/**
+ * Stop playing the sound and disallow any further events from happening.
+ */
 SMSound.prototype.stop = function() {
     this._stopTimedEvents = true; //Don't allow any timed events to be thrown after the music is stopped
-    if (this._sound != null) {
+    if (this._sound !== null) {
         this._sound.stop();
     }
 };
 
+/**
+ * @return {Boolean} whether the soundmanager sound has actually been loaded
+ * or not.
+ */
 SMSound.prototype.isLoaded = function() {
-    return this._sound != null;
+    return this._sound !== null;
 };
 
-
+/**
+ * @return {Boolean} true if the sound is playing right now.
+ */
 SMSound.prototype.isPlaying = function() {
-    return (this._sound != null && this._sound.playState === 1);
+    return (this._sound !== null && this._sound.playState === 1);
 };
 
+/**
+ * @return {Boolean} true if the sound is ready to play
+ */
 SMSound.prototype.isReady = function() {
     return (this._sound.readyState === 3);
 };
 
+/**
+ * @return {Boolean} true if there was an error parsing the sound file.
+ */
 SMSound.prototype.errorFlag = function() {
     return (this._sound.readyState === 2);
 };
 
+/**
+ * Return a description of the error that happened with the sound. Right now,
+ * this simply says that the sound failed to load, but could be extended to
+ * use other readable error strings as well, if needed.
+ * @return {string|null} The error string if there was an error, null otherwise
+ */
 SMSound.prototype.getError = function() {
     if (this.errorFlag()) {
         return "Sound failed to load.";
@@ -151,18 +189,30 @@ SMSound.prototype.getError = function() {
 Sound.prototype.registerEventHandler = function(eventName, eventHandler) {
     var handlerIndex = this._eventTypes.indexOf(eventName);
     if (handlerIndex != -1) {
-        this._eventHandlers[handlerIndex] = eventHandler;
+        this._eventHandlers[eventName] = eventHandler;
     }
 };
 
+/**
+ * Push a timed event (an object with keys time and eventHandler, where the
+ * handler is executed at the specified time) onto the internal queue of timed
+ * events. If we have a sound, actually tell the sound to do the eventHandler
+ * at the specified time.
+ * @param {int} time time to execute the handler, in milliseconds
+ * @param {function():undefined} eventHandler the handler to execute
+ */
 SMSound.prototype.addTimedEvent = function(time, eventHandler) {
     var newEvent = {time: time, eventHandler: eventHandler};
     this._timedEvents.push(newEvent);
-    if (this._sound != null) {
+    if (this._sound !== null) {
         this._installOneTimedEvent(newEvent);
     }
 };
 
+/**
+ * Tell the soundmanager sound not to execute any more timed events, and reset
+ * the queue of timed events that this Sound knows about.
+ */
 SMSound.prototype.clearTimedEvents = function() {
     for (var index = 0; index < this._timedEvents.length; index++) {
         this._sound.clearOnPosition(this._timedEvents[index].time);
@@ -171,8 +221,9 @@ SMSound.prototype.clearTimedEvents = function() {
 };
 
 /**
- * Installs all timed events to the current soundmanager
- * sound object.
+ * Installs all timed events to the current soundmanager (i.e. tells the
+ * soundmanager sound that it should execute all the timed events at their
+ * respective times).
  */
 SMSound.prototype._installTimedEvents = function() {
     for (var index = 0; index < this._timedEvents.length; index++) {
@@ -188,15 +239,15 @@ SMSound.prototype._installOneTimedEvent = function(timedEvent) {
     var _this = this;
     var eventTime = timedEvent.time;
     var newEventHandler = function() {
-        //If we don't add this if clause, soundmanager will throw timed 
-        //events when we don't want it to (specifically, it will continue
-        //to throw some events after we stop playing, and it will throw
-        //events between the start of the audio and the position where
-        //we started playing the audio
-        if (!_this._stopTimedEvents && eventTime > _this._startTime) { 
+        // If we don't add this if clause, soundmanager will throw timed 
+        // events when we don't want it to (specifically, it will continue
+        // to throw some events after we stop playing, and it will throw
+        // events between the start of the audio and the position where
+        // we started playing the audio
+        if (!_this._stopTimedEvents && eventTime > _this._startTime) {
             timedEvent.eventHandler();
         }
-    }
+    };
     this._sound.onPosition(timedEvent.time, newEventHandler);
 };
 
@@ -207,9 +258,8 @@ SMSound.prototype._installOneTimedEvent = function(timedEvent) {
  *   with the handler to call.
  */
 SMSound.prototype._callEventHandler = function(eventName) {
-    var index = this._eventTypes.indexOf(eventName);
-    if (index != -1 && this._eventHandlers[index] != null) {
-        this._eventHandlers[index]();
+    if (this._eventHandlers[eventName]) {
+        this._eventHandlers[eventName]();
     }
 };
 
@@ -236,7 +286,7 @@ SMSound.prototype._makeEventRouter = function(eventName) {
  * An event handler for when the music finished.
  */
 SMSound.prototype._handleFinished = function() {
-    this._stopTimedEvents = true; //Make sure that no timed events are thrown after the music ends
+    this._stopTimedEvents = true; // Make sure that no timed events are thrown after the music ends
     this._callEventHandler("finished");
 };
 
