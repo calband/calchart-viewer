0225402ec6380e303806400ffbd377b2d7b107c3
Brandon Chinn <brandonchinn178@berkeley.edu>
2014-09-12 01:10:24 -0700

    Initial pdfGenerator code

---
+++ js/pdf/main.js
@@ -0,0 +1,32 @@
+/**
+ * @fileOverview This file will generate a PDF representation of dots and movements
+ */
+
+var jsPDF = require("../vendor/jsPDF");
+
+/**
+ * The pdfGenerator will be the overall object that contains the methods to generate
+ * a PDF.
+ *
+ * @param {Show} show
+ * @param {String} dot
+ *
+ * @return {jsPDF} the PDF document for the dot
+ */
+var pdfGenerator = function(show, dot) {
+    this._show = show;
+    this._dot = dot;
+    this._pdf = jsPDF();
+}
+
+/**
+ * generatePDF will generate a PDF for a specific dot, containing its movements,
+ * positions, and continuities relevant to it.
+ *
+ * @return {jsPDF} the PDF document for the dot
+ */
+pdfGenerator.prototype.generatePDF = function() {
+    return this._pdf.output();
+};
+
+module.exports = pdfGenerator;
