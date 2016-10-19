dfc9f3f3a461b56bbf8c46a1b953bba490fb5b5f
Brandon Chinn <brandonchinn178@gmail.com>
2016-10-19 11:58:34 -0700

    add push script for demo site

---
+++ scripts/push_demo.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## Automatically pushes the current branch to the calchart-viewer-demo repo, where
+## the branch could be viewed at calband.github.io/calchart-viewer-demo.
+
+# http://stackoverflow.com/a/12142066/4966649
+CURR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+echo "Force creation of a new branch..."
+if git branch | grep -q demo-master; then
+    git branch -qD demo-master
+fi
+git checkout -qb demo-master
+
+echo "Editing .gitignore..."
+rm .gitignore
+echo "node_modules" > .gitignore
+
+echo "Building and committing static files..."
+grunt build > /dev/null # hide output
+git add . > /dev/null # hide output
+git commit -qm "Build static files for demo"
+
+echo "Pushing to demo repo..."
+if ! git remote | grep -q demo; then
+    # have user do it just in case user does not have permission to add calchart-viewer-demo
+    echo "Please add the demo repo with 'git remote add demo <git-url>'"
+    return 1
+fi
+git push -fq demo demo-master:master
+
+echo "Cleaning up..."
+git checkout -q $CURR_BRANCH
+git branch -Dq demo-master
+
+echo "done."
