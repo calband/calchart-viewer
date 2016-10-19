8ae25487cd67864b3de608bdf9d0fa7d1b080495
Brandon Chinn <brandonchinn178@gmail.com>
2016-10-18 20:52:06 -0700

    add dots to mobile

--- js/mobile.js
+++ js/mobile.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
     // rescale grapher to correct aspect ratio
     var width = $(".graph-container").outerWidth();
     $(".graph-container .graph").css("height", width / 1.5);
+
+    // choosing a dot now goes directly to the individual continuity page
+    $(".js-dot-labels").change(function() {
+        var show = $(".js-select-show").val();
+        var dot = $(this).val();
+        var defaults = "&md-orientation=west&bev-orientation=east&sd-orientation=east&layout-order=ltr&endsheet-widget=md";
+        window.location.href = "pdf.html?show=" + show + "&dots=" + dot + defaults;
+    });
 });
