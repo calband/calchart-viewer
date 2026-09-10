e72466254cdeb047dc434dfb5557cd2fe5abb581
Jaehyeon Lee <149716119+Doolyvader@users.noreply.github.com>
2026-09-09 21:17:58 -0700

    Add DotNav field selector

--- js/mobile.js
+++ js/mobile.js
@@ -1,7 +1,29 @@
+var FieldOrientation = require("./viewer/FieldOrientation");
+
 window.isMobile = true;
 
 $(document).ready(function() {
     // rescale grapher to correct aspect ratio
     var width = $(".graph-container").outerWidth();
     $(".graph-container .graph").css("height", width / 1.5);
+
+    var selectedVenue = null;
+
+    $(".js-dotnav-field").change(function() {
+        var venueId = $(this).val();
+
+        selectedVenue = FieldOrientation.VENUES[venueId] || null;
+
+        if (selectedVenue === null) {
+            console.log("[DotNav] No field selected");
+            return;
+        }
+
+        console.log(
+            "[DotNav] Field:",
+            selectedVenue.name,
+            "East heading:",
+            selectedVenue.eastHeading
+        );
+    });
 });
